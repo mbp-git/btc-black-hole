efb9982e4ed887190247194817d450ac9e5c2aef
mpb-git <mpb.git@pm.me>
2024-11-24 13:29:03 -0800

    working in progress

--- btc-black-hole-rust/src/main.rs
+++ btc-black-hole-rust/src/main.rs
@@ -1,16 +1,18 @@
 // src/main.rs
 
 use btc_black_hole_rust::BruteForceApp;
+use eframe::NativeOptions;
 use eframe::egui::Vec2;
 
 fn main() {
     let app = BruteForceApp::default();
-    let native_options = eframe::NativeOptions {
-        initial_window_size: Some(Vec2::new(900.0, 750.0)),
+    let native_options = NativeOptions {
+        initial_window_size: Some(Vec2::new(1200.0, 800.0)),
         ..Default::default()
     };
+
     eframe::run_native(
-        "BTC Black Hole Rust",
+        "BTC Black Hole Brute Force",
         native_options,
         Box::new(|_cc| Box::new(app)),
     )
